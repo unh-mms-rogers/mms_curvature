e8f9852577e5d5e31ca71032608c2d5ee7507169
AJR <anthony.j.rogers@gmail.com>
2026-03-24 01:52:45 -0500

    Modified sanity checks in mms_Grad_RV to allow for differences up to 1e-17.  (now with divB check too)  -AJR

--- mms_curvature.py
+++ mms_curvature.py
@@ -288,7 +288,7 @@ def mms_Grad_RV(postimes=None, posvalues=None, magtimes=None, magvalues=None, no
 
     assert np.allclose(divB,
                        np.trace(gradB, axis1=-2, axis2=-1),
-                       atol=0, equal_nan=True), 'Calculated divergence differs from trace of calculated gradient!'
+                       atol=1.0e-17, equal_nan=True), 'Calculated divergence differs from trace of calculated gradient!'
     assert np.allclose(curlB,
                        np.einsum('ijk,...jk', LevCiv3, gradB),
                        atol=1.0e-17, equal_nan=True), 'Calculated curl differs from Levi-Civita permutated gradient!'
